11e7e84ad9682a7b42808281b109179133695679
jbaxter6 <jbaxter6@radford.edu>
2020-10-15 12:46:50 -0400

    migrations created

--- db/migrate/20201015162503_create_scratchers.rb
+++ db/migrate/20201015162503_create_scratchers.rb
@@ -1,6 +1,10 @@
 class CreateScratchers < ActiveRecord::Migration[6.0]
   def change
     create_table :scratchers do |t|
+      t.string :item_name
+      t.string :item_description
+      t.string :item_size
+      t.string :item_cost
 
       t.timestamps
     end
